84bf2dc32963b176b5e3795fcee887bc25c32230
mpdomon <martin.domon@gmail.com>
2024-04-17 14:00:21 +0200

    doc reset

--- Grupp2MVC/Program.cs
+++ Grupp2MVC/Program.cs
@@ -15,16 +15,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-//Console.WriteLine("My name is Maria.");
-Console.WriteLine("namaste, mero nama Mattias ho. ho ho.  ");
 app.UseRouting();
 
 app.UseAuthorization();
-Console.WriteLine("so it begins");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-// bla bla lba /Martin
