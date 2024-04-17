9f1a341097ac17661d2028f019f902d9969dec10
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-04-17 15:00:02 +0200

    Added EntityFrameWorkCore, scaffolded Vehicles controller and views and a DbContext class

---
+++ Grupp2MVC/Data/Grupp2MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Grupp2MVC.Models;
+
+namespace Grupp2MVC.Data
+{
+    public class Grupp2MVCContext : DbContext
+    {
+        public Grupp2MVCContext (DbContextOptions<Grupp2MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grupp2MVC.Models.Vehicle> Vehicle { get; set; } = default!;
+    }
+}
