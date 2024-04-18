9cf7cef13fdc6ee8c058a4aa4ff13ee70c36bf46
mpdomon <martin.domon@gmail.com>
2024-04-18 15:38:43 +0200

    added improvements

--- Grupp2MVC/Models/Vehicle.cs
+++ Grupp2MVC/Models/Vehicle.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Grupp2MVC.Models
 {
     public class Vehicle
     {
         public int Id { get; set; }
+        [DisplayName("Type of vehicle")]
         public VehicleType VehicleType { get; set; }
+        [DisplayName("Registration Number")]
         public string RegistrationNumber { get; set; }
         public string Color { get; set; }
         [StringLength(20)]
@@ -13,8 +16,11 @@ namespace Grupp2MVC.Models
         [StringLength(20)]
         public string Model { get; set; }
         [Range(0,10)]
+        [DisplayName("Number of wheels")]
         public int AmountOfWheels { get; set; }
+        [DisplayName("Arrival")]
         public DateTime TimeOfArrival { get; set; }
+        [DisplayName("Departure")]
         public DateTime? TimeOfDeparture { get; set; }
     }
 }
