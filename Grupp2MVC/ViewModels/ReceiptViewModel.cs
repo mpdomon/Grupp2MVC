e9ed1947fb407c8731fa4323a027d9d25732f43e
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-04-18 14:28:09 +0200

    #6 - Added view and view model for displaying receipt after checkout

---
+++ Grupp2MVC/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Grupp2MVC.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string RegistrationNumber { get; set; }
+        public double Price { get; set; }
+        public DateTime TimeOfArrival { get; set; }
+        public DateTime TimeOfDeparture { get; set; }
+    }
+}
