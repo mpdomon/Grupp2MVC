bc1177c8925c1ca8d46b773f8658c2a85807a637
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-04-19 13:05:13 +0200

    #6 Added Receipt entity

---
+++ Grupp2MVC/Models/Receipt.cs
@@ -0,0 +1,14 @@
+﻿namespace Grupp2MVC.Models
+{
+    public class Receipt
+    {
+        //public string Make { get; set; }
+        //public string Model { get; set; }
+        //public string RegistrationNumber { get; set; }
+        public double Price { get; set; }
+        public DateTime TimeOfArrival { get; set; }
+        public DateTime TimeOfDeparture { get; set; }
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
